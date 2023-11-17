576691de7dffdab6daca735f949401fbda825deb
Jørgen Christian Heinsen <jhe@trifork.com>
2023-11-17 10:24:30 +0100

    Corrected libraryVersion to match newest published version.

--- kemdk/build.gradle.kts
+++ kemdk/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libraryGroupId = "dk.gls"
 val libraryArtifactId = "kotlin-emdk"
-val libraryVersion = "0.0.1"
+val libraryVersion = "1.0.1"
 
 android {
     namespace = "dk.gls.kemdk"
