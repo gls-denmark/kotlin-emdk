059a764c2b5af379421db9cd5280efc5c8883bc6
Jørgen Christian Heinsen <jhe@trifork.com>
2024-09-27 13:14:44 +0200

    Added try catch and check for whether continuation isCompleted, trying to avoid IllegalStateException.

--- kemdk/src/main/java/dk/gls/kemdk/emdk/EMDKIntegration.kt
+++ kemdk/src/main/java/dk/gls/kemdk/emdk/EMDKIntegration.kt
@@ -1,13 +1,16 @@
 package dk.gls.kemdk.emdk
 
 import android.content.Context
+import android.util.Log
 import com.symbol.emdk.EMDKManager
 import dk.gls.kemdk.EMDKResult
 import dk.gls.kemdk.model.toEMDKConfigResult
 import dk.gls.kemdk.model.toEMDKResult
+import dk.gls.kemdk.oemInfo.OEMInfoRequester.Companion.TAG
 import dk.gls.kemdk.toKEMDKFailure
 import dk.gls.kemdk.toKEMDKSuccess
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 
 class EMDKIntegration(private val context: Context) : IEMDKIntegration {
@@ -15,11 +18,15 @@ class EMDKIntegration(private val context: Context) : IEMDKIntegration {
     override suspend fun configure(): EMDKResult<Unit, EMDKThrowable> = suspendCancellableCoroutine { continuation ->
         val emdkListenerWrapper = object : IEMDKListenerWrapper {
             override fun onCompleted() {
-                continuation.resume(Unit.toKEMDKSuccess())
+                if(!continuation.isCompleted) {
+                    continuation.safeResume(Unit.toKEMDKSuccess())
+                }
             }
 
             override fun onError(throwable: EMDKThrowable) {
-                continuation.resume(throwable.toKEMDKFailure())
+                if(!continuation.isCompleted) {
+                    continuation.safeResume(throwable.toKEMDKFailure())
+                }
             }
         }
 
@@ -36,5 +43,13 @@ class EMDKIntegration(private val context: Context) : IEMDKIntegration {
             continuation.resume(res)
         }
 
+    }
+}
+
+private fun <T> Continuation<T>.safeResume(value: T) {
+    try {
+        resume(value)
+    } catch (illegalStateException: IllegalStateException) {
+        Log.e("safeResume", "IllegalStateException $illegalStateException")
     }
 }
